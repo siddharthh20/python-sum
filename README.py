4aabeeaf3858c6e74c4e6b09ab9a7d3e740c0435
Siddharth Sigh <83107315+sidsomya12@users.noreply.github.com>
2021-06-22 11:36:08 +0530

    Update and rename README.md to README.py

---
+++ README.py
@@ -0,0 +1,9 @@
+var1 = "54"
+var4 = "32"
+var2 = 4
+var3 = 36.7
+print("Enter first number")
+n1 = input()
+print("Enter second number")
+n2 = input()
+print("Sum of these two numbers is", int(n1) + int(n2))
